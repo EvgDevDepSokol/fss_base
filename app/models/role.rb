099a9231832f089bb82e0303722278d70f57967a
sergeykarpov83 <sergey.karpov55@gmail.com>
2016-02-17 13:44:07 +0300

    поэкспериментировал с table_role_rights

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,3 +1,9 @@
 class Role < ActiveRecord::Base
+  self.table_name = 'roles'
+  self.primary_key = 'roleID'
 
+  def custom_hash
+    serializable_hash
+  end
 end
+
--- app/models/table_role_right.rb
+++ app/models/table_role_right.rb
@@ -3,9 +3,22 @@ class TableRoleRight < ActiveRecord::Base
   self.table_name = 'table_role_rights'
   self.primary_key = 'tableID'
 
+  belongs_to :role, foreign_key: 'roleID'
+  belongs_to :tablelist, foreign_key: 'tableID'
+
+  alias_attribute :role_id, :roleID
+  alias_attribute :tablelist_id, :tableID
+
   # todo: fix primary key
   def custom_hash
-    serializable_hash
+    serializable_hash(include: {
+      role:{only: [:role]},
+      tablelist:{only: [:table]}
+    })
   end
+
+#  def serializable_hash(options={})
+#    super options.merge(methods: :id)
+#  end
 
 end
