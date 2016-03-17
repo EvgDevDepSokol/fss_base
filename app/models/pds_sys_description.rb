331effcf6025a24a394a525a8ad51fda1e12c2f0
sergeykarpov83 <sergey.karpov55@gmail.com>
2016-03-17 12:14:50 +0300

    Поправил pds_sys_description для селектора системы в зависимости от проекта

--- app/models/pds_sys_description.rb
+++ app/models/pds_sys_description.rb
@@ -2,4 +2,12 @@ class PdsSysDescription < ActiveRecord::Base
 
   self.table_name = 'pds_sys_description'
 
+  belongs_to :pds_project, foreign_key: 'Project'
+  belongs_to :system, foreign_key: :sys, class_name: 'PdsSyslist'
+
+  def custom_hash
+    serializable_hash(include: {
+        system: {only: [:System]}})
+  end
+
 end
