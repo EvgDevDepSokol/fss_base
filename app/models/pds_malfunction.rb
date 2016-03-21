b8b472aef7ce54862875338f92d6b84d233c69ed
sergeykarpov83 <sergey.karpov55@gmail.com>
2016-03-21 19:04:04 +0300

    поправил malfunctions

--- app/models/pds_malfunction.rb
+++ app/models/pds_malfunction.rb
@@ -1,14 +1,28 @@
 class PdsMalfunction < ActiveRecord::Base
 
-  self.inheritance_column = nil
+  #self.inheritance_column = nil
+  self.inheritance_column = :_type_disabled
   self.table_name = 'pds_malfunction'
   belongs_to :system, foreign_key: :sys, class_name: 'PdsSyslist'
+  belongs_to :pds_project_unit, foreign_key: 'regidity_unitid'
+  
   alias_attribute :system_id, :sys
+  alias_attribute :pds_project_unit_id, :regidity_unitid
 
   has_many :items, class_name: 'PdsMalfunctionDim', foreign_key: :Malfunction
 
-  def serializable_hash(options={})
-    super.merge({id: id, system: system.to_s})
+#  def serializable_hash(options={})
+#    super options.merge(methods: :id)
+#  end
+
+#  def serializable_hash(options={})
+#    super.merge({id: id, system: system.to_s})
+#  end
+
+  def custom_hash
+    serializable_hash(include: {
+        pds_project_unit: {only: [], include: {unit: {only: :Unit_RU}}}
+    })
   end
 
   # todo: add with language
@@ -16,8 +30,8 @@ class PdsMalfunction < ActiveRecord::Base
   # служебных строки в виде:
   # @DESC1(1 часть описания (не длиннее 66 символов, разбивается по словам))
   # @DESC2(продолжение описания)
-  def description
-    Desc
-  end
+#  def description
+#    Desc
+#  end
 
 end
