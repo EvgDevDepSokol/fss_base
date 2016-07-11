d8bbfe7e247c65b34accf66f0f339fcb13c20df3
sergeykarpov83 <sergey.karpov55@gmail.com>
2016-07-11 18:47:05 +0300

    перевел projectSettingsController под BaseController

--- app/controllers/project_settings_controller.rb
+++ app/controllers/project_settings_controller.rb
@@ -1,42 +1,43 @@
-class ProjectSettingsController < ApplicationController
-  include GeneralControllerHelper
-  layout 'layouts/table'
-  require 'csv'
+#class ProjectSettingsController < ApplicationController
+class ProjectSettingsController < BaseController
+#  include GeneralControllerHelper
+#  layout 'layouts/table'
+#  require 'csv'
 
   ACTIONS = [:pds_eng_on_sys, :pds_project_unit, :pds_doc_on_sys,
              :pds_project_sys, :week_report, :pds_documents,
              :pds_documentation, :pds_simplifications, :pds_queries,
              :pds_sys_description, :pds_dr, :pds_mathmodel].freeze
 
-  before_action :project, :current_user_rights
+#  before_action :project, :current_user_rights
 
-  helper_method :project, :table_data, :table_header, :editable_properties, :model_class
+#  helper_method :project, :table_data, :table_header, :editable_properties, :model_class
 
-  def index
-    # Rails.logger.info params
-    # для таблиц где есть ProjectID мы хотим видеть данные в контексте
-    # данного проекта, для остальных пока отображаем все данные
-    if model.new.respond_to?(:Project)
-      @data_list = model.where(Project: project.ProjectID).limit(10_000)
-    else
-      @data_list = model.limit(10_000)
-    end
-
-    respond_to do |format|
-      format.html { render index_view }
-      format.csv
-      format.xlsx
-    end
-  end
-
-  def index_view
-    if template_exists?("project_settings/#{model_path}")
-      "project_settings/#{model_path}"
-    else
-      :index
-    end
-  end
-  helper_method :index_view
+#  def index
+#    # Rails.logger.info params
+#    # для таблиц где есть ProjectID мы хотим видеть данные в контексте
+#    # данного проекта, для остальных пока отображаем все данные
+#    if model.new.respond_to?(:Project)
+#      @data_list = model.where(Project: project.ProjectID).limit(10_000)
+#    else
+#      @data_list = model.limit(10_000)
+#    end
+#
+#    respond_to do |format|
+#      format.html { render index_view }
+#      format.csv
+#      format.xlsx
+#    end
+#  end
+#
+#  def index_view
+#    if template_exists?("project_settings/#{model_path}")
+#      "project_settings/#{model_path}"
+#    else
+#      :index
+#    end
+#  end
+#  helper_method :index_view
 
   def pds_eng_on_sys
     @data_list = PdsEngOnSy.where(Project: project.ProjectID)
@@ -45,7 +46,7 @@ class ProjectSettingsController < ApplicationController
 
   # TODO: не ясно как быть с выбором
   def pds_project_units
-    @data_list = PdsProjectUnit.where(Project: project.ProjectID).includes(:unit)
+    @data_list = PdsProjectUnit.where(Project: project.ProjectID).includes(:pds_unit)
   end
 
   def pds_doc_on_sys
@@ -79,93 +80,93 @@ class ProjectSettingsController < ApplicationController
     @data_list = PdsSysDescription.where(Project: project.ProjectID).includes(:system)
   end
 
-  def create
-    @pds_button = PdsButton.new permit_params
-
-    if @pds_button.save
-      format.json { render :show, status: :created, location: @pds_button }
-    else
-      format.json { render json: @pds_button.errors, status: :unprocessable_entity }
-    end
-  end
-
-  def update
-    Rails.logger.warn permit_params
-
-    if current_object.update permit_params
-
-      render json: { status: :ok, data: current_object.custom_hash }
-    else
-      render json: { errors: current_object.errors, data: current_object.reload.custom_hash },
-             status: :unprocessable_entity
-    end
-
-  rescue
-    render json: { errors: current_object.errors, data: current_object.reload.custom_hash },
-           status: :unprocessable_entity
-  end
-
-  def destroy
-    # @pds_button.destroy
-    if true
-      render json: {}, head: :no_content
-    else
-      render json: { errors: [] }, status: 403
-    end
-  end
-
-  private
-
-  # Use callbacks to share common setup or constraints between actions.
-  def set_pds_button
-    @current_object = model.find(params[:id])
-  end
-
-  def current_object
-    @current_object ||= model.find(params[:id])
-  end
-
-  def project
-    @project ||= PdsProject.find(params[:pds_project_id])
-  end
-
-  def permit_params
-    params.require(model.to_s.underscore).permit!
-  end
-
-  def editable_properties
-    if model_class.respond_to?(:editable_attributes)
-      model_class.editable_attributes.to_json
-    else
-      model_class.attribute_names.reduce({}) do |hash, attr|
-        hash.merge(
-          attr => { type: model_class.columns_hash[attr].type,
-                    title: attr })
-      end.to_json
-    end
-  end
-
-  def table_header
-    if model_class.respond_to?(:view_columns)
-      model_class.view_columns
-    else
-      model_class.attribute_names.map { |attr| { property: attr, header: attr } }.to_json
-    end
-  end
-
-  def table_data
-    if model_class.method_defined? :custom_hash
-      @data_list.map(&:custom_hash).to_json
-    else
-      @data_list.to_json
-    end
-  end
-
-  def current_user_rights
-    if (current_user.user_rights >= 3)
-      true
-    else
-      render html: "<strong>Не хватает прав для просмотра таблицы.</strong>".html_safe
-    end
-  end
+#  def create
+#    @pds_button = PdsButton.new permit_params
+#
+#    if @pds_button.save
+#      format.json { render :show, status: :created, location: @pds_button }
+#    else
+#      format.json { render json: @pds_button.errors, status: :unprocessable_entity }
+#    end
+#  end
+#
+#  def update
+#    Rails.logger.warn permit_params
+#
+#    if current_object.update permit_params
+#
+#      render json: { status: :ok, data: current_object.custom_hash }
+#    else
+#      render json: { errors: current_object.errors, data: current_object.reload.custom_hash },
+#             status: :unprocessable_entity
+#    end
+#
+#  rescue
+#    render json: { errors: current_object.errors, data: current_object.reload.custom_hash },
+#           status: :unprocessable_entity
+#  end
+#
+#  def destroy
+#    # @pds_button.destroy
+#    if true
+#      render json: {}, head: :no_content
+#    else
+#      render json: { errors: [] }, status: 403
+#    end
+#  end
+#
+#  private
+#
+#  # Use callbacks to share common setup or constraints between actions.
+#  def set_pds_button
+#    @current_object = model.find(params[:id])
+#  end
+#
+#  def current_object
+#    @current_object ||= model.find(params[:id])
+#  end
+#
+#  def project
+#    @project ||= PdsProject.find(params[:pds_project_id])
+#  end
+#
+#  def permit_params
+#    params.require(model.to_s.underscore).permit!
+#  end
+#
+#  def editable_properties
+#    if model_class.respond_to?(:editable_attributes)
+#      model_class.editable_attributes.to_json
+#    else
+#      model_class.attribute_names.reduce({}) do |hash, attr|
+#        hash.merge(
+#          attr => { type: model_class.columns_hash[attr].type,
+#                    title: attr })
+#      end.to_json
+#    end
+#  end
+#
+#  def table_header
+#    if model_class.respond_to?(:view_columns)
+#      model_class.view_columns
+#    else
+#      model_class.attribute_names.map { |attr| { property: attr, header: attr } }.to_json
+#    end
+#  end
+#
+#  def table_data
+#    if model_class.method_defined? :custom_hash
+#      @data_list.map(&:custom_hash).to_json
+#    else
+#      @data_list.to_json
+#    end
+#  end
+#
+#  def current_user_rights
+#    if (current_user.user_rights >= 3)
+#      true
+#    else
+#      render html: "<strong>Не хватает прав для просмотра таблицы.</strong>".html_safe
+#    end
+#  end
 end
