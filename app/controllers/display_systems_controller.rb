7cd47dc143a398dfdc646e9c63758172e3e84fa7
sergeykarpov83 <sergey.karpov55@gmail.com>
2016-08-01 16:28:41 +0300

    поправил отображение датчиков в аналоговых и дискретных системах отображения

--- app/controllers/display_systems_controller.rb
+++ app/controllers/display_systems_controller.rb
@@ -5,22 +5,24 @@ class DisplaySystemsController < BaseController
   # уродливо, но ОООЧЕНЬ быстро. Было 35-38 секунд, стало 3-5 секунд!
   def pds_ppcas
     @data_list = PdsPpca.where(Project: project.ProjectID)
-                        .includes(:system).pluck('ppcID', 'pds_syslist.System', 'pds_syslist.SystemID', 'Shifr', 'Key', 'identif',
-                                                 'Description', 'Description_EN', 'Detector', 'L_lim', 'U_lim', 'Unit', 'nom')
+                        .includes(:system, :pds_detector).pluck('ppcID', 'pds_syslist.SystemID','pds_syslist.System',  'Shifr', 'Key', 'identif',
+                        'Description', 'Description_EN',
+                        'L_lim', 'U_lim', 'Unit', 'nom',
+                        'pds_detectors.DetID','pds_detectors.tag')
     @data_list = @data_list.each.map do |e|
       e1 = {}
       e1['id']               = e[0]
-      e1['system']           = { System: e[1], id: e[2] }
+      e1['system']           = { id: e[1], System: e[2]}
       e1['Shifr']            = e[3]
       e1['Key']              = e[4]
       e1['identif']          = e[5]
       e1['Description']      = e[6]
       e1['Description_EN']   = e[7]
-      e1['Detector']         = e[8]
-      e1['L_lim']            = e[9]
-      e1['U_lim']            = e[10]
-      e1['Unit']             = e[11]
-      e1['nom']              = e[12]
+      e1['L_lim']            = e[8]
+      e1['U_lim']            = e[9]
+      e1['Unit']             = e[10]
+      e1['nom']              = e[11]
+      e1['pds_detector']     = { id: e[12], tag: e[13]}
       e = e1
     end
   end
@@ -28,8 +30,9 @@ class DisplaySystemsController < BaseController
   # TODO: сделать разбиение запроса на части, а то долго отдается
   def pds_ppcds
     @data_list = PdsPpcd.where(Project: project.ProjectID)
-                        .includes(:system).pluck('ppcdID', 'pds_syslist.System', 'pds_syslist.SystemID', 'Shifr', 'Key', 'identif',
-                                                 'Description', 'Description_EN', 'Detector')
+                        .includes(:system, :pds_detector).pluck('ppcdID', 'pds_syslist.System', 'pds_syslist.SystemID', 'Shifr', 'Key', 'identif',
+                                  'Description', 'Description_EN',
+                        'pds_detectors.DetID','pds_detectors.tag')
     @data_list = @data_list.each.map do |e|
       e1 = {}
       e1['id']               = e[0]
@@ -39,7 +42,7 @@ class DisplaySystemsController < BaseController
       e1['identif']          = e[5]
       e1['Description']      = e[6]
       e1['Description_EN']   = e[7]
-      e1['Detector']         = e[8]
+      e1['pds_detector']     = { id: e[8], tag: e[9]}
       e = e1
     end
   end
--- app/models/pds_ppca.rb
+++ app/models/pds_ppca.rb
@@ -1,7 +1,9 @@
 class PdsPpca < ActiveRecord::Base
   self.table_name = 'pds_ppca'
   belongs_to :system, foreign_key: :sys, class_name: 'PdsSyslist'
+  belongs_to :pds_detector, foreign_key: :Detector
   alias_attribute :system_id, :sys
+  alias_attribute :pds_detector_id, :Detector
 
   def custom_map
     true
@@ -9,7 +11,9 @@ class PdsPpca < ActiveRecord::Base
 
   def custom_hash
     serializable_hash(include: {
-                        system: { only: :System } })
+                        system: { only: :System }, 
+                        pds_detector: { only: :tag }
+                               })
   end
 
   def serializable_hash(options = {})
