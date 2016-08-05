829452a9af2cf0ed7d41238a7cc7bc5d361513ee
sergeykarpov83 <sergey.karpov55@gmail.com>
2016-08-05 14:52:03 +0300

    сделал static_valve_types_selector еще красивее

--- app/assets/javascripts/selectors/static_valve_types.jsx
+++ app/assets/javascripts/selectors/static_valve_types.jsx
@@ -7,30 +7,36 @@ var Select = require('react-select');
 var onChange = require('../selectors/selectors.jsx').onChange;
 const VALVE_TYPES = require('../selectors/data/valve_types.js')
 
-module.exports = React.createClass({
-  displayName: 'ValveTypeSelector',
+var StaticSelector = React.createClass({
 
   getInitialState() {
     var value = null;
     var label = this.props.value;
-    VALVE_TYPES.forEach(function (obj) {
+    const OPTIONS = this.props.options;
+    OPTIONS.forEach(function (obj) {
       if (obj.label == label) {
         value = obj.value;
       }
     })
     return {
-      value: value
+      value: value,
     };
   },
 
   setValue(value) {
-    onChange(value,this)
+    var _this = this.props._this;
+    this.setState({value: value});
+    var h = {};
+    h[_this.props.attribute] = value;
+    _this.props.onValue(h);
   },
 
   render: function() {
     return (
-      React.createElement(Select, {name: "ValveType",
-        options: VALVE_TYPES,
+      React.createElement(Select, {
+        displayName: this.props.displayName,
+        name: this.props.name,
+        options: this.props.options,
         onChange: this.setValue,
         value: this.state.value,
         simpleValue:true,
@@ -40,3 +46,18 @@ module.exports = React.createClass({
     );
   }
 });
+
+var ValveTypeSelector = React.createClass({
+  displayName: 'ValveTypeSelector',
+  render: function() {
+    return (
+      <StaticSelector  value = {this.props.value} this = {this}
+        options = {VALVE_TYPES}
+        displayName = 'ValveTypeSelector'
+        name = 'ValveType'
+      />
+    )
+  }
+});
+
+module.exports = ValveTypeSelector;
