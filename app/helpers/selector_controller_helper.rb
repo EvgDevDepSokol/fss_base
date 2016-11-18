f1a6f357dda9338d1f1f0c52c768473df0f8e971
sergeykarpov83 <sergey.karpov55@gmail.com>
2016-11-18 17:42:04 +0300

    селектор контроллер хелпер

---
+++ app/helpers/selector_controller_helper.rb
@@ -0,0 +1,18 @@
+module SelectorControllerHelper
+  def do_format(hash)
+    hash = hash.each.map do |tmp|
+      tmp1 = {}
+      tmp1['value']=tmp[0]
+      tmp1['label']=tmp[1]
+      tmp=tmp1
+    end
+
+    respond_to do |format|
+      format.json { render json: Oj.dump(hash) }
+    end
+  end
+
+  def project
+    @project ||= PdsProject.find_by(ProjectID: params[:pds_project_id])
+  end
+end
