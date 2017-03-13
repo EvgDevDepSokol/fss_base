ebb0ca6c73c842e1efea723334db807c158618f9
sergeykarpov83 <sergey.karpov55@gmail.com>
2017-03-13 15:04:18 +0300

    добавил проверку уникальности ключа в базе при импорте

--- app/assets/javascripts/components/xlsx-import/step4.jsx
+++ app/assets/javascripts/components/xlsx-import/step4.jsx
@@ -51,100 +51,111 @@ var ImportStep4 = React.createClass({
     var importHeaders = this.props.columns;
     var importData = this.props.importData; 
     var msg = this.props.msg;
+    var message = null;
+    var filters = null; 
+    var to_exit = this.state.to_exit;
+    var modalTableContainerClass = "modal-table-container"
 
     if(this.props.isOpen) {
+      if (msg.length==1 && msg[0].not_unique) {
+        filters = <div>Файл обработан.</div>;
+        message = <p>'Значение выбранного ключевого поля в базе для этой таблицы не уникально! Записи для импорта не могут быть определены однозначно. Импорт не может быть продолжен.'</p>;
+        modalTableContainerClass = "modal-table-container hidden-element"
+        to_exit = true;
+      } else {
 
-      var radio_err = < div className = 'filter-err-radio-group modal-filter' >
-        <p> Ошибки</p>
-        <input type='radio' name='filter-err' value='0' checked={this.state.filter_err === 0} onChange={this.onFilterErrChange}/>Показать все < br />
-        <input type='radio' name='filter-err' value='1' checked={this.state.filter_err === 1} onChange={this.onFilterErrChange}/>Показать только записи с ошибками < br />
-        <input type='radio' name='filter-err' value='2' checked={this.state.filter_err === 2} onChange={this.onFilterErrChange}/>Показать только записи без ошибок < br />
-      </div> 
+        var radio_err = < div className = 'filter-err-radio-group modal-filter' >
+          <p> Ошибки</p>
+          <input type='radio' name='filter-err' value='0' checked={this.state.filter_err === 0} onChange={this.onFilterErrChange}/>Показать все < br />
+          <input type='radio' name='filter-err' value='1' checked={this.state.filter_err === 1} onChange={this.onFilterErrChange}/>Показать только записи с ошибками < br />
+          <input type='radio' name='filter-err' value='2' checked={this.state.filter_err === 2} onChange={this.onFilterErrChange}/>Показать только записи без ошибок < br />
+        </div> 
 
-      var radio_add = < div className = 'filter-add-radio-group modal-filter' >
-        <p> Статус</p>
-        <input type='radio' name='filter-add' value='0' checked={this.state.filter_add === 0} onChange={this.onFilterAddChange}/>Показать все < br />
-        <input type='radio' name='filter-add' value='1' checked={this.state.filter_add === 1} onChange={this.onFilterAddChange}/>Показать только записи на добавление < br />
-        <input type='radio' name='filter-add' value='2' checked={this.state.filter_add === 2} onChange={this.onFilterAddChange}/>Показать только записи на обновление < br />
-      </div> 
+        var radio_add = < div className = 'filter-add-radio-group modal-filter' >
+          <p> Статус</p>
+          <input type='radio' name='filter-add' value='0' checked={this.state.filter_add === 0} onChange={this.onFilterAddChange}/>Показать все < br />
+          <input type='radio' name='filter-add' value='1' checked={this.state.filter_add === 1} onChange={this.onFilterAddChange}/>Показать только записи на добавление < br />
+          <input type='radio' name='filter-add' value='2' checked={this.state.filter_add === 2} onChange={this.onFilterAddChange}/>Показать только записи на обновление < br />
+        </div> 
 
-      importHeaders['Статус'] = '';
-      importHeaders['Ошибки'] = '';
-      importHeaders['Результат'] = '';
+        importHeaders['Статус'] = '';
+        importHeaders['Ошибки'] = '';
+        importHeaders['Результат'] = '';
 
-      if (importHeaders != null) {
-        var headersFrom = Object.keys(importHeaders).map(function(key, i) {
+        if (importHeaders != null) {
+          var headersFrom = Object.keys(importHeaders).map(function(key, i) {
 
-          return (
-            <th key={i + '-header'} className={"static-header"}>
-              {key}
-            </th>
-          );
-        });
-      };
-
-      var filtered = [];
-      var filter_add = this.state.filter_add;
-      var filter_err = this.state.filter_err;
-
-      var rows = null;
-      if (importData.length > 0 && msg.length > 0) {
-        rows = importData.map(function(row, i) {
-          row['Статус']=msg[i].add;
-          row['Ошибки']=msg[i].err;
-          row['Результат']=msg[i].result;
-          return row;
-        });
-
-        switch(filter_add){
-          case 1:
-            rows = rows.filter(function(row){
-              return row['Статус'];
-            });
-            break;
-          case 2:
-            rows = rows.filter(function(row){
-              return !row['Статус'];
-            });
-            break;
-        };
-  
-        switch(filter_err){
-          case 1:
-            rows = rows.filter(function(row){
-              return !!row['Ошибки'];
-            });
-            break;
-          case 2:
-            rows = rows.filter(function(row){
-              return !row['Ошибки'];
-            });
-            break;
-        };
-
-        rows = rows.map(function(row, i) {
-          row['Статус']=row['Статус']?'Новая запись':'Существующая запись';
-
-          var cells = Object.keys(importHeaders).map(function(key, j) {
             return (
-              <td key={i + '-' + j + '-cell'}>
-                {row[key]}
-              </td>
+              <th key={i + '-header'} className={"static-header"}>
+                {key}
+              </th>
             );
           });
+        };
 
-          return (
-            <tr key={i + '-row'}>
-              {cells}
-            </tr>
-          );
-        });
-        var filters = !!msg?<div className='modal-filter-container'>{radio_add}{radio_err}</div>:<div></div>;
-      } else {
-        var filters = <div>Ваш файл обрабатывается</div>;
+        var filtered = [];
+        var filter_add = this.state.filter_add;
+        var filter_err = this.state.filter_err;
+
+        var rows = null;
+        if (importData.length > 0 && msg.length > 0) {
+          rows = importData.map(function(row, i) {
+            row['Статус']=msg[i].add;
+            row['Ошибки']=msg[i].err;
+            row['Результат']=msg[i].result;
+            return row;
+          });
+
+          switch(filter_add){
+            case 1:
+              rows = rows.filter(function(row){
+                return row['Статус'];
+              });
+              break;
+            case 2:
+              rows = rows.filter(function(row){
+                return !row['Статус'];
+              });
+              break;
+          };
+  
+          switch(filter_err){
+            case 1:
+              rows = rows.filter(function(row){
+                return !!row['Ошибки'];
+              });
+              break;
+            case 2:
+              rows = rows.filter(function(row){
+                return !row['Ошибки'];
+              });
+              break;
+          };
+
+          rows = rows.map(function(row, i) {
+            row['Статус']=row['Статус']?'Новая запись':'Существующая запись';
+
+            var cells = Object.keys(importHeaders).map(function(key, j) {
+              return (
+                <td key={i + '-' + j + '-cell'}>
+                  {row[key]}
+                </td>
+              );
+            });
+
+            return (
+              <tr key={i + '-row'}>
+                {cells}
+              </tr>
+            );
+          });
+          filters = !!msg?<div className='modal-filter-container'>{radio_add}{radio_err}</div>:<div></div>;
+        } else {
+          filters = <div>Ваш файл обрабатывается</div>;
+        }
       }
-      var next_button=this.state.to_exit?null:<button onClick={this.nextModal}>Подтвердить</button>
-      var exit_button=this.state.to_exit?<button onClick={this.closeModal}>Выход</button>:<button onClick={this.closeModal}>Отмена</button>
+      var next_button=to_exit?null:<button onClick={this.nextModal}>Подтвердить</button>
+      var exit_button=to_exit?<button onClick={this.closeModal}>Выход</button>:<button onClick={this.closeModal}>Отмена</button>
     };
 
 
@@ -153,7 +164,7 @@ var ImportStep4 = React.createClass({
         <Modal isOpen={this.props.isOpen} onRequestClose={this.closeModal} style={this.props.style} contentLabel={this.props.contentLabel} onAfterOpen={this.afterOpenModal}>
           <h2>Предварительные результаты</h2>
           {filters}
-          <div className="modal-table-container" key={"modal-table"}>
+          <div className={modalTableContainerClass} key={"modal-table"}>
             <table className={"table table-bordered table-striped table-hover"}>
               <thead>
                 <tr>
@@ -167,6 +178,7 @@ var ImportStep4 = React.createClass({
           </div>
           <p>Всего {!!rows?rows.length:0} строк данных.</p>
 
+          <div className={'modal-warning'}>{message}</div>
           {exit_button} 
           {next_button} 
         </Modal>
--- app/controllers/import_controller.rb
+++ app/controllers/import_controller.rb
@@ -6,23 +6,27 @@ class ImportController < ApplicationController
 
   def update_all_check
     message = []
-    params[:data].each do |i, row|
-      msg={add: false, result: '', err: ''}
-      @permitted = row.permit!
-      unless !!current_object(row[@key_column])
-        msg[:add]=true
-        @current_object=model.new
+    if key_column_unique? then
+      params[:data].each do |i, row|
+        msg={add: false, result: '', err: ''}
+        @permitted = row.permit!
+        unless !!current_object(row[@key_column])
+          msg[:add]=true
+          @current_object=model.new
+        end
+        @current_object.attributes=@permitted
+        if (!!@project) then
+          @current_object.Project=@project.id
+        end
+        if (!@current_object.valid?) then
+          msg[:err]=@current_object.errors.messages
+        elsif(!@current_object.changed?) then
+          msg[:err]='Аттрибуты не меняются!'
+        end
+        message.push(msg)
       end
-      @current_object.attributes=@permitted
-      if (!!@project) then
-        @current_object.Project=@project.id
-      end
-      if (!@current_object.valid?) then
-        msg[:err]=@current_object.errors.messages
-      elsif(!@current_object.changed?) then
-        msg[:err]='Аттрибуты не меняются!'
-      end
-      message.push(msg)
+    else
+      message.push({not_unique: true, err: ''})
     end
     render json: { status: :ok, message: message}
   rescue
@@ -33,7 +37,6 @@ class ImportController < ApplicationController
     message = []
     params[:data].each do |i, row|
       msg={add: false, result: '', err:''}
-      byebug
       @permitted = row.permit!
       unless !!current_object(row[@key_column])
         msg[:add]=true
@@ -70,6 +73,17 @@ class ImportController < ApplicationController
 
   def key_column
     @key_column = (params[:keyColumn] if params[:keyColumn])
+  end
+
+  def key_column_unique?
+   if !!@project
+      cnt_all  = model.where(Project: @project).count(@key_column)
+      cnt_dist = model.where(Project: @project).count('distinct('+@key_column+')')
+    else
+      cnt_all  = model.count(@key_column)
+      cnt_dist = model.count('distinct('+@key_column+')')
+    end
+    return cnt_all==cnt_dist
   end
 
   def project
