0008fd08e18b8a56b23cfae17ec3f647c8e2f67d
sergeykarpov83 <sergey.karpov55@gmail.com>
2017-04-10 14:27:26 +0300

    поправил hw_ped_selector

--- app/assets/javascripts/selectors/hw_ped.jsx
+++ app/assets/javascripts/selectors/hw_ped.jsx
@@ -6,7 +6,7 @@ var React = require('react');
 var Select = require('react-select');
 var onChange = require('../selectors/selectors.jsx').onChange;
 var getSelectorOptions = require('../selectors/selectors.jsx').getSelectorOptions;
-var path = '/api/hw_peds';
+var path = '/selectors/hw_peds';
 module.exports = React.createClass({
   displayName: 'HwPedSelector',
 
@@ -30,7 +30,6 @@ module.exports = React.createClass({
           {pds_project_id:project.ProjectID},
           this
         );
-        options = $.map(options , function(el){ return {value: el.id, label: el.ped} } )
 
         callback(null, {
           options: options,
@@ -60,6 +59,5 @@ module.exports.options = function(){
     {pds_project_id:project.ProjectID},
     this
   );
-  options = $.map(options , function(el){ return {value: el.id, label: el.ped} } )
   return options;
 };
---
+++ app/controllers/selectors/hw_peds_controller.rb
@@ -0,0 +1,9 @@
+class Selectors::HwPedsController < ApplicationController
+  include SelectorControllerHelper
+
+  def index
+    @hw_peds = HwPed.where(Project: project.ProjectID).order(:ped)
+    .pluck(:ped_N, :ped)
+    do_format(@hw_peds)
+  end
+end
