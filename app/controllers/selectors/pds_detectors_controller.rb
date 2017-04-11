a4b69b78c1ac683446655b1eacd4e446556a9f8f
sergeykarpov83 <sergey.karpov55@gmail.com>
2017-04-11 10:50:51 +0300

    убрал из селектора по детекторам пустые записи

--- app/controllers/selectors/pds_detectors_controller.rb
+++ app/controllers/selectors/pds_detectors_controller.rb
@@ -1,9 +1,10 @@
 class Selectors::PdsDetectorsController < ApplicationController
   include SelectorControllerHelper
-  before_action :project
 
   def index
-    @pds_detectors = PdsDetector.where(Project: project.ProjectID).order(:tag)
+    @pds_detectors = PdsDetector.where(Project: project.ProjectID)
+                                .where.not(tag: '')
+                                .order(:tag)
                                 .pluck(:DetID, :tag)
     do_format(@pds_detectors)
   end
