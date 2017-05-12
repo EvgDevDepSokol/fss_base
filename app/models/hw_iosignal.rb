31ef1f795c11312490c4fe1ebb0d82ee4e762665
sergeykarpov83 <sergey.karpov55@gmail.com>
2017-05-12 11:18:35 +0300

    запретил изменение автогенерящихся hw_iosignal

--- app/models/hw_iosignal.rb
+++ app/models/hw_iosignal.rb
@@ -7,10 +7,34 @@ class HwIosignal < ApplicationRecord
   alias_attribute :hw_ped_id, :pedID
   alias_attribute :hw_iosignaldef_id, :signID
 
+  before_destroy :check_for_genextsig
+  before_create :check_for_genextsig
+  before_save :check_for_genextsig_signid
+
   def custom_hash
     serializable_hash(include: {
                         hw_ped: { only: [:ped] },
                         hw_iosignaldef: { only: [:ioname] }
                       })
   end
+
+  private
+
+  def check_for_genextsig
+    hw_ped = HwPed.where(id: hw_ped_id).first
+    if hw_ped.GenExtSig == 'да'
+      errors.add(:base, :check_for_genextsig_err, { ped: hw_ped.ped } )
+      throw(:abort)
+    end
+  end
+
+  def check_for_genextsig_signid
+    hw_ped = HwPed.where(id: hw_ped_id).first
+    if ((hw_ped.GenExtSig == 'да') && (signID!=signID_was))
+      errors.add(:base, :check_for_genextsig_signid_err, { ped: hw_ped.ped } )
+      throw(:abort)
+    end
+  end
+
+
 end
