fb5bb895d8cb513459cdbb8a1931479fd4728c19
sergeykarpov83 <sergey.karpov55@gmail.com>
2018-01-11 17:09:56 +0300

    pds_sys_description alias fixed

--- app/models/pds_sys_description.rb
+++ app/models/pds_sys_description.rb
@@ -5,6 +5,8 @@ class PdsSysDescription < ApplicationRecord
   belongs_to :pds_project, foreign_key: 'Project'
   belongs_to :system, foreign_key: :sys, class_name: 'PdsSyslist'
 
+  alias_attribute :system_id, :sys
+
   def custom_hash
     serializable_hash(include: {
                         system: { only: [:System] }
