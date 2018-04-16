42878ebe343c3956d3aff7617dc12ba35190584a
sergeykarpov83 <sergey.karpov55@gmail.com>
2018-04-16 12:45:59 +0300

    added some flags

--- config/application.rb
+++ config/application.rb
@@ -18,8 +18,8 @@ module FSS_database
 
     # config.web_console.whitelisted_ips = '10.0.226.8'
 
-    config.browserify_rails.commandline_options = ['--fast']
-    #config.browserify_rails.commandline_options = ['-t [ babelify --presets [ 2015 react ]]', '--fast', '--extension jsx']
+    #config.browserify_rails.commandline_options = ['--fast']
+    config.browserify_rails.commandline_options = ['-t [ babelify --presets [ 2015 react ]]', '--extension jsx']
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
