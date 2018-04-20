dd947fec8745a2a34ea4747e26f97798e60b3ba2
developer <no@email.here>
2018-04-20 15:39:17 +0300

    копирую import_export.png вручную в production.sh

--- production.sh
+++ production.sh
@@ -2,4 +2,5 @@ rake assets:clean
 rake assets:clobber
 rm -rf ./tmp/cache/
 RAILS_ENV=production rake assets:precompile
+cp ./app/assets/images/import_export.png ./public/assets/
 rails s --binding=0.0.0.0 -e production &
