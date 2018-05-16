b7e83d27d17bab1373263be7e2c17b200c124fb9
developer <no@email.here>
2018-05-16 15:28:01 +0300

    незначительные изменения

---
+++ app/assets/javascripts/selectors/system_all.jsx
@@ -0,0 +1,64 @@
+// selector to be used for systems for all projects
+
+'use strict';
+
+var React = require('react');
+var Select = require('react-select');
+var onChange = require('../selectors/selectors.jsx').onChange;
+var getSelectorOptions = require('../selectors/selectors.jsx').getSelectorOptions;
+var path = '/selectors/pds_syslists';
+
+module.exports = React.createClass({
+  displayName: 'SystemAllSelector',
+
+  propTypes: {label: PropTypes.string},
+
+  getInitialState() {
+    return {
+      value: this.props.id,
+      disabled: this.props.disabled
+    };
+  },
+
+  setValue(value) {
+    onChange(value,this)
+  },
+
+  render: function() {
+
+    var getOptions = function(input, callback) {
+      setTimeout(function() {
+        var options = getSelectorOptions(
+          path,
+          {},
+          this
+        );
+        callback(null, {
+          options: options,
+          complete: true,
+        });
+      }, 0);
+    };
+
+    return (
+      React.createElement(Select.Async, {name: "System",
+        loadOptions: getOptions,
+        onChange: this.setValue,
+        value: this.state.value,
+        simpleValue:true,
+        multi: false,
+        disabled: this.props.disabled,
+        clearable: false,
+        cache: false
+        })
+    );
+  }
+});
+module.exports.options = function(){
+  var options = getSelectorOptions(
+    path,
+    {},
+    this
+  );
+  return options;
+};
