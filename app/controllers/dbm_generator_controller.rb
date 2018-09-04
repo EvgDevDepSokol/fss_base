e6054fe5f26ca7b20fd910852d10e1ead46cf6ef
developer <no@email.here>
2018-09-04 15:12:25 +0300

    rubocop

--- app/controllers/dbm_generator_controller.rb
+++ app/controllers/dbm_generator_controller.rb
@@ -5,7 +5,7 @@ class DbmGeneratorController < ApplicationController
 
   TEMPLATE_PATH = Rails.root.join('app', 'views', 'workers', 'dbm_generator')
   FILE_PATH = '/home/shared/'.freeze
-  REMOTE_FOLDER = ['gen_rf/', 'gen_mf/', 'gen_peds/', 'gen_ppc/', 'gen_ann/']
+  REMOTE_FOLDER = ['gen_rf/', 'gen_mf/', 'gen_peds/', 'gen_ppc/', 'gen_ann/'].freeze
 
   def prepare_hash
     hash = params[:data]
@@ -18,7 +18,7 @@ class DbmGeneratorController < ApplicationController
       render_sel_rf(dbm_generator)
     when '1'
       render_sel_mf(dbm_generator)
-    when '2','4'
+    when '2', '4'
       render_sel_ped(dbm_generator)
     end
     render json: { status: :ok }
@@ -156,11 +156,11 @@ class DbmGeneratorController < ApplicationController
           hw_ped = hw_ic.hw_ped
           data_obj = ''
           if table_id.to_i == 4 # announciators
-            if dbm_generator.gen_type == '2'
-              path = 'sel_ped_announciators1.sel.erb'
-            else
-              path = 'sel_ped_announciators2.sel.erb'
-            end
+            path = if dbm_generator.gen_type == '2'
+                     'sel_ped_announciators1.sel.erb'
+                   else
+                     'sel_ped_announciators2.sel.erb'
+                   end
             data_obj = Tilt.new(TEMPLATE_PATH.join(path).to_s).render(
               ActionView::Base.new, dbm_generator.as_json.merge(hw_ic: hw_ic, hw_ped: hw_ped, obj: obj, is_rus: is_rus)
             )
