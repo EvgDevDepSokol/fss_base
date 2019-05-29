bc336d8ffb9b29cb8a752472ba797775bbbd40af
developer <developer@project.db.server>
2019-05-29 16:36:52 +0300

    добавил линковку last в deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,7 @@
 ./clean_ass.sh
 ssh deploy@10.0.104.151 "cd fss_db_releases && mkdir '$(date +%Y%m%d)'" 
 ssh deploy@10.0.104.151 "cd fss_db_releases/'$(date +%Y%m%d)' && mkdir fss_base" 
+ssh deploy@10.0.104.151 "cd fss_db_releases && rm last" 
+ssh deploy@10.0.104.151 "cd fss_db_releases && ln -s '$(date +%Y%m%d)'/fss_base/ last" 
 scp -r ~/work/fss_base/* deploy@10.0.104.151:/home/deploy/fss_db_releases/$(date +%Y%m%d)/fss_base/
 ssh deploy@10.0.104.151 "cd ~/fss_db_releases/'$(date +%Y%m%d)'/fss_base && ./stop.sh && ./update_start.sh"
