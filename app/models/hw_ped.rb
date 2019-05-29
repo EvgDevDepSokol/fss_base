c9509357bf6719c45e60cb3f0344ae85f30bd305
developer <developer@project.db.server>
2019-05-29 16:38:16 +0300

    поправил генерацию iosignals при сохранении ped

--- app/models/hw_ped.rb
+++ app/models/hw_ped.rb
@@ -47,29 +47,27 @@ class HwPed < ApplicationRecord
   end
 
   after_save do |hw_ped|
-    if hw_ped.GenExtSig == 'да'
-      SIGNAL_ARRAY.each do |sig_name|
-        sig_id = HwIosignaldef.where(ioname: sig_name).first.id
-        hw_iosignals = HwIosignal.where(pedID: hw_ped.id, signID: sig_id).order(:id).to_a
-        icnt = hw_ped[sig_name] - hw_iosignals.size
-        icnt = 0 if hw_ped[sig_name] < 0
-        if icnt > 0
-          while icnt > 0
-            hw_iosignal = HwIosignal.new
-            hw_iosignal.Project = hw_ped.Project
-            hw_iosignal.pedID = hw_ped.id
-            hw_iosignal.signID = sig_id
-            hw_iosignal.skip_check_ped
-            hw_iosignal.save
-            icnt -= 1
-          end
-        elsif icnt < 0
-          while icnt < 0
-            hw_iosignal = HwIosignal.where(pedID: hw_ped.id, signID: sig_id).order(:id).last
-            hw_iosignal.skip_check_ped
-            hw_iosignal.destroy
-            icnt += 1
-          end
+    SIGNAL_ARRAY.each do |sig_name|
+      sig_id = HwIosignaldef.where(ioname: sig_name).first.id
+      hw_iosignals = HwIosignal.where(pedID: hw_ped.id, signID: sig_id).order(:id).to_a
+      icnt = hw_ped[sig_name] - hw_iosignals.size
+      icnt = 0 if hw_ped[sig_name] < 0
+      if icnt > 0
+        while icnt > 0
+          hw_iosignal = HwIosignal.new
+          hw_iosignal.Project = hw_ped.Project
+          hw_iosignal.pedID = hw_ped.id
+          hw_iosignal.signID = sig_id
+          hw_iosignal.skip_check_ped
+          hw_iosignal.save
+          icnt -= 1
+        end
+      elsif icnt < 0
+        while icnt < 0
+          hw_iosignal = HwIosignal.where(pedID: hw_ped.id, signID: sig_id).order(:id).last
+          hw_iosignal.skip_check_ped
+          hw_iosignal.destroy
+          icnt += 1
         end
       end
     end
